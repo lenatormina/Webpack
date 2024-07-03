8efd72e496b402497b57009387230d658397e725
lenatormina <90458810+lenatormina@users.noreply.github.com>
2024-07-03 22:54:11 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+import "./index.css";
+import JS_IMAGE from "./assets/image.png";
+
+const jsImageHTML = document.createElement("img");
+jsImageHTML.src = JS_IMAGE;
+
+document.body.append(jsImageHTML);
